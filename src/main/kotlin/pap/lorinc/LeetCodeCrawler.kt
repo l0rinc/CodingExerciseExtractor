d5da42b7f87d27b6ed2575e7d88a2e07a21db74c
Pap Lőrinc <paplorinc@gmail.com>
2017-04-08 15:12:39 +0300

    Added simple LeetCode crawler

--- src/main/kotlin/pap/lorinc/LeetCodeCrawler.kt
+++ src/main/kotlin/pap/lorinc/LeetCodeCrawler.kt
@@ -1,14 +1,15 @@
 package pap.lorinc
 
+import com.beust.klaxon.*
 import org.jsoup.Connection
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Element
-import java.lang.Long.parseLong
+import org.jsoup.nodes.Document
+import pap.lorinc.Crawler.echo
 import java.time.Duration
 import java.time.LocalDateTime
 
 enum class Language { CPP, JAVA, PYTHON, C, CSHARP, JAVASCRIPT, RUBY, SWIFT, GOLANG }
-data class LeetCodeProblem(val submitTime: LocalDateTime, val url: String, val name: String, val runTime: Duration, val language: Language)
+data class LeetCodeProblem(val submitTime: LocalDateTime, val packageName: String, val link: String, val description: String, val solution: String, val name: String, val runTime: Duration, val language: Language)
 
 object Crawler2 {
     val base = "https://leetcode.com"
@@ -17,63 +18,140 @@ object Crawler2 {
     var password = "" // your password here
 
     @JvmStatic fun main(args: Array<String>) {
+        val document = login()
+        val contents = parseContent(document)
+        val commands = generateCommands(contents)
+        println(commands())
+    }
+
+    private fun parseContent(document: Connection.Response): List<LeetCodeProblem> {
+        val visited = hashSetOf<String>()
+        val results = mutableListOf<LeetCodeProblem>()
+        for (page in 1..10000) {
+            val parsedSubmissions = submissions(page, document.cookies())
+
+            val solutions = solutions(document, parsedSubmissions, visited)
+            results.addAll(solutions)
+
+            if (parsedSubmissions.boolean("has_next") != true)
+                break
+        }
+        return results.reversed()
+    }
+
+    private fun generateCommands(contents: List<LeetCodeProblem>) = {
+        val before = """
+            >
+            >git init
+            >
+            """.trimMargin(">")
+
+        val git = contents.map { info ->
+            val mainJava = "src/main/java/leetcode/${info.packageName}"
+            val className = className(info)
+            """
+            >
+            >mkdir -p '$mainJava'
+            >${echo(generateMain(info))} > $mainJava/$className.java
+            >git add src
+            >git commit -m "${info.name}" --date="${info.submitTime}"
+            >
+            """.trimMargin(">")
+        }.joinToString("\n")
+
+        val userName = userId.replace(Regex("@.+$"), "")
+        val languages = contents.map { it.language.toString().toLowerCase() }.toSet().joinToString(",")
+        val after = """
+            >
+            >
+            >${echo("to install gradle, type: sudo add-apt-repository ppa:cwchien/gradle && sudo apt-get update && sudo apt-get install gradle")}
+            >${echo("[![Build Status](https://travis-ci.org/$userName/LeetCodeSolutions.png)](https://travis-ci.org/$userName/LeetCodeSolutions)\n\nSolutions to my [LeetCode](http://LeetCode.com) exercises, exported by [CodingExerciseExtractor](https://github.com/paplorinc/CodingExerciseExtractor).")} > README.md
+            >${echo("language: $languages\n\njdk: oraclejdk8\n\nbefore_install: chmod +x gradlew\nscript: ./gradlew clean build --stacktrace")} > .travis.yml
+            >gradle init --type java-library --test-framework spock && rm src/test/groovy/LibraryTest.groovy && rm src/main/java/Library.java && git add -A && gradle build
+            >
+            """.trimMargin(">")
+        before + git + after
+    }
+
+    private fun generateMain(info: LeetCodeProblem) =
+            """
+        >package leetcode.${info.packageName};
+        >
+        >import java.util.*;
+        >
+        >/**
+        > * ${info.description.lines().map(String::trim).joinToString("\n * ")}
+        > * Source: ${info.link}
+        > */
+        >${info.solution}
+        >""".trimMargin(">").trim()
+
+
+    private fun solutions(document: Connection.Response, parsedSubmissions: JsonObject, visited: HashSet<String>): List<LeetCodeProblem> =
+            parsedSubmissions.array<JsonObject>("submissions_dump")!!
+                    .filter { s -> s.string("status_display") == "Accepted" }
+                    .filter { s -> visited.add(s.string("title")!!) }
+                    .map { submission ->
+                        val solution = Jsoup.connect(base + submission.string("url")).cookies(document.cookies()).get()
+                        LeetCodeProblem(
+                                submitTime = parseDuration(submission),
+                                packageName = parsePackage(submission),
+                                link = parseLink(solution),
+                                description = getDescription(solution),
+                                solution = getSolution(solution),
+                                name = getName(submission),
+                                runTime = getRunTime(submission),
+                                language = parseLanguage(submission)
+                        )
+                    }
+
+    private fun submissions(page: Int, cookies: MutableMap<String, String>): JsonObject {
+        val submissions = Jsoup.connect("$base/api/submissions/my/$page/?format=json").cookies(cookies).ignoreContentType(true).get().body().text()
+        val parsedSubmissions = Parser().parse(StringBuilder(submissions)) as JsonObject
+        return parsedSubmissions
+    }
+
+    private fun login(): Connection.Response {
         val loginForm = Jsoup
-                .connect("$base/accounts/login/")
+                .connect("$base/accounts/login")
                 .execute()
 
-        System.out.println(loginForm.cookies())
-
-        val token = loginForm.parse().select("input[name=\"csrfmiddlewaretoken\"]").attr("value")
-        val document = Jsoup
-                .connect("$base/accounts/login/")
+        val token = loginForm.parse().select("""input[name="csrfmiddlewaretoken"]""").attr("value")
+        return Jsoup
+                .connect("$base/accounts/login")
                 .method(Connection.Method.POST)
-                .referrer("$base/accounts/login/")
+                .referrer("$base/accounts/login")
                 .data("csrfmiddlewaretoken", token)
                 .data("login", userId)
                 .data("password", password)
                 .cookies(loginForm.cookies())
                 .execute()
-
-        System.out.println(document)
-
-        val submissions = Jsoup
-                .connect("$base/submissions/")
-                .method(Connection.Method.GET)
-                .cookies(document.cookies())
-                .execute()
-                .parse()
-
-        val lines = submissions.select("tbody > tr")
-                .filter { r -> r.child(2).text().contains("Accepted") }
-                .map { prob ->
-                    val (submitTime, question, _, runTime, language) = prob.children()
-                    LeetCodeProblem(
-                            submitTime = parseDuration(submitTime),
-                            url = getUrl(question),
-                            name = getName(question),
-                            runTime = getRunTime(runTime),
-                            language = parseLanguage(language)
-                    )
-                }
-
-        System.out.println(lines)
     }
 
-    private fun parseDuration(trim: Element): LocalDateTime =
-            Regex("^(?:(\\d+) years?)?(?: *(\\d+) months?)?(?: *(\\d+) weeks?)?(?: *(\\d+) +days?)?(?: *(\\d+) hours?)?(?: *(\\d+) minutes?)?\\s+ago$")
-                    .matchEntire(trim.text().replace(Regex("\\W+"), " ")).let {
+    private fun className(info: LeetCodeProblem) = Regex("""public class (\w+)""").find(info.solution)!!.groupValues[1]
+
+    private fun parseDuration(submission: JsonObject): LocalDateTime =
+            Regex("""^(?:(\d+) years?)?(?: *(\d+) months?)?(?: *(\d+) weeks?)?(?: *(\d+) +days?)?(?: *(\d+) hours?)?(?: *(\d+) minutes?)?$""")
+                    .matchEntire(submission.string("time")!!.replace(Regex("""\W+"""), " ")).let {
                 val (year, month, week, day, hour, minute) = it!!.destructured
                 LocalDateTime.now()
-                        .plusYears(year.toLongOrNull() ?: 0)
-                        .plusMonths(month.toLongOrNull() ?: 0)
-                        .plusWeeks(week.toLongOrNull() ?: 0)
-                        .plusDays(day.toLongOrNull() ?: 0)
-                        .plusHours(hour.toLongOrNull() ?: 0)
-                        .plusMinutes(minute.toLongOrNull() ?: 0)
+                        .minusYears(year.toLongOrNull() ?: 0)
+                        .minusMonths(month.toLongOrNull() ?: 0)
+                        .minusWeeks(week.toLongOrNull() ?: 0)
+                        .minusDays(day.toLongOrNull() ?: 0)
+                        .minusHours(hour.toLongOrNull() ?: 0)
+                        .minusMinutes(minute.toLongOrNull() ?: 0)
             }
 
-    private fun getUrl(question: Element): String = base + question.select("a").attr("href")
-    private fun getName(question: Element): String = question.select("a").text()
-    private fun getRunTime(runTime: Element): Duration = Duration.ofMillis(parseLong(runTime.text().replace(Regex("\\D+"), "")))
-    private fun parseLanguage(s: Element) = Language.valueOf(s.text().toUpperCase())
+    private fun parsePackage(submission: JsonObject): String = submission.string("title")!!.trim().toLowerCase().replace(Regex(""" +(.)""")) { x -> x.groupValues[1].toUpperCase() }
+    private fun getDescription(solution: Document): String = solution.select("""meta[name="description"]""").attr("content")
+    private fun parseLink(solution: Document): String = base + solution.select("""a[href^="/problems/"]""").first().attr("href")
+    private fun getSolution(solution: Document): String {
+        val code = Regex("submissionCode: '(.+)',").find(solution.select("script")[7].html())!!.groupValues[1]
+        return code.replace(Regex("""\\u(....)""")) { m -> m.groupValues[1].toLong(16).toChar().toString() }
+    }
+
+    private fun getName(submission: JsonObject): String = submission.string("title")!!
+    private fun getRunTime(submission: JsonObject): Duration = Duration.ofMillis(submission.string("runtime")!!.replace(Regex("""\D+"""), "").toLong())
+    private fun parseLanguage(submission: JsonObject): Language = Language.valueOf(submission.string("lang")!!.toUpperCase())
 }
